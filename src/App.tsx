ddc909c5afe27cc98e692dde3b001a6a5c1ee0d3
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-12 19:11:23 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import './App.css';
 import { Todo } from './Todo';
 
 function App() {
   return (
-    <div className="App">
-          Learn React
+    <div >
       <Todo />
     </div>
   );
