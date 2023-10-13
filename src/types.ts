3ee29e4461daf885d0c1c224c2e8ffd7a367ff06
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-12 19:12:34 -0600

    Add TodoListProps type

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@
 export interface TodoType {
     id: string;
     content: string;
+    completed: boolean;
 }
 
 /**
@@ -15,4 +16,22 @@ export interface TodoInputProps {
      * @param todo The new todo item.
      */
     onAddTodo: (todo: TodoType) => void;
+}
+
+/**
+ * Props for the TodoList component.
+ */
+
+export interface TodoListProps {
+
+    /**
+     * The list of todo items.
+     */
+    todos: TodoType[];
+
+    /**
+     * Callback function to be called when a todo item is toggled.
+     * @param id The id of the todo item.
+     */
+    onToggleItem: (id: string) => void;
 }
