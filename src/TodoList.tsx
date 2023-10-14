1e365efce24139bdab1649bf3cf582e48800b27e
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-14 17:22:29 -0600

    Add onDeleteItem props

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,16 +1,24 @@
 import {TodoListProps, TodoType} from './types';
 
-const TodoList = ({todos, onToggleItem}: TodoListProps) => {
+const TodoList = ({todos, onToggleItem, onDeleteItem}: TodoListProps) => {
     return (
         <>
         {todos.map((todo :TodoType) => (
             <div
-                className="todo-item"
                 key={todo.id}
-                data-completed={todo.completed}
-                onClick={() => onToggleItem(todo.id)}
+                className="todo-item-container"
             >
-            {todo.content}</div>
+            <span 
+                className="todo-item"
+                onClick={() => onToggleItem(todo.id)}
+                data-completed={todo.completed}>
+                {todo.content}</span>
+            <button
+                className="delete-button"
+                data-testid="delete-button"
+                onClick={() => onDeleteItem(todo)}
+            />
+            </div>
         ))}
         </>
     )
