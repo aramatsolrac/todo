2323936c374686d22dab640f64df71eb61be0868
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-14 19:59:35 -0600

    Import CategoryList

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,42 +1,30 @@
 import { TodoList } from './TodoList';
 import { TodoInput } from './TodoInput';
 import { useTodos } from './useTodos';
-import './Todo.css';
 import { TodoType } from './types';
+import { CategoryList } from './CategoryList';
+import './Todo.css';
 
+const Todo = ({ items }: { items?: TodoType[]; }) => {
+    const {
+        displayTodos, addTodo, toggleItem, deleteItem, setCategory, categories } = useTodos(items);
 
-const Todo = ({items}: {items?: TodoType[]}) => {
-    const {displayTodos, addTodo, toggleItem, deleteItem, setCategory} = useTodos(items);
+    const { total, active, completed } = categories;
 
     return (
-    <div className='todo-container'>
-        <h2 className='todo-header'>Todos</h2>
-        <TodoInput onAddTodo={addTodo} />
-        <div className='categories-container'>
-            <button
-                className='button total'
-                data-testid='todo-total'
-                onClick={() => setCategory('total')}
-                >Total</button>
-            <button
-                className='button active'
-                data-testid='todo-active'
-                onClick={() => setCategory('active')}
-                >Active</button>
-            <button
-                className='button completed'
-                data-testid='todo-completed'
-                onClick={() => setCategory('completed')}
-                >Completed</button>
+        <div className='todo-container'>
+            <h2 className='todo-header'>Todos</h2>
+            <TodoInput onAddTodo={addTodo} />
+            <CategoryList
+                categories={{ total, active, completed }}
+                switchCategory={setCategory}
+            />
+            <TodoList
+                todos={displayTodos}
+                onToggleItem={toggleItem}
+                onDeleteItem={deleteItem} />
         </div>
-        <TodoList
-            todos={displayTodos
-            }
-            onToggleItem={toggleItem}
-            onDeleteItem={deleteItem}
-        />
-    </div>
-  );
-}
+    );
+};
 
-export {Todo}
+export { Todo };
