64bd9b67534d01d445f4afc8002e142bda6acae2
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-15 17:25:30 -0600

    Add SearchInput

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -4,10 +4,17 @@ import { useTodos } from './useTodos';
 import { TodoType } from './types';
 import { CategoryList } from './CategoryList';
 import './Todo.css';
+import { SearchInput } from './SearchInput';
 
 const Todo = ({ items }: { items?: TodoType[]; }) => {
     const {
-        displayTodos, addTodo, toggleItem, deleteItem, setCategory, categories } = useTodos(items);
+        displayTodos,
+        addTodo,
+        toggleItem,
+        deleteItem,
+        setCategory,
+        categories,
+        search } = useTodos(items);
 
     const { total, active, completed } = categories;
 
@@ -15,6 +22,7 @@ const Todo = ({ items }: { items?: TodoType[]; }) => {
         <div className='todo-container'>
             <h2 className='todo-header'>Todos</h2>
             <TodoInput onAddTodo={addTodo} />
+            <SearchInput onSearch={search} />
             <CategoryList
                 categories={{ total, active, completed }}
                 switchCategory={setCategory}
@@ -23,6 +31,7 @@ const Todo = ({ items }: { items?: TodoType[]; }) => {
                 todos={displayTodos}
                 onToggleItem={toggleItem}
                 onDeleteItem={deleteItem} />
+
         </div>
     );
 };
