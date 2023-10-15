b8a21146bd8e42f0df67e768082cfdfd821e35e7
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2023-10-14 19:59:07 -0600

    Create Category.tsx

---
+++ src/Category.tsx
@@ -0,0 +1,16 @@
+import {CategoryProps} from './types';
+
+export const Category = ({ label, number, type, switchCategory }: CategoryProps) => {
+    return (
+        <div className='categories-container'>
+            <label className='categories-label'>{label}
+                <button
+                    className={`button ${type}`}
+                    data-testid={`todo-${type}`}
+                    onClick={() => switchCategory(type)}
+                >{number > 0 ? number : '0'}
+                </button>
+            </label>
+        </div>
+    );
+};
